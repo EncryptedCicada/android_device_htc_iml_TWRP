4bdfc21328a98e9960131667f0c941ff60d0c9d3
Encrypted Cicada <dev.varnit@gmail.com>
2024-05-27 15:44:42 +0000

    Booting tree. Mounts not set correctly. This is a skeleton

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_htc_iml.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_htc_iml-user \
+    omni_htc_iml-userdebug \
+    omni_htc_iml-eng
