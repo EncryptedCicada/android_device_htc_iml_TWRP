083f3d0c3b0af4201fb2a68febbe4fcd1e0efbf2
EncryptedCicada <dev.varnit@gmail.com>
2024-08-17 00:36:57 +0530

    Blacklist device that is identified as mouse to (try to) hide cursor.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,5 +80,6 @@ TARGET_SCREEN_WIDTH=1080
 TARGET_SCREEN_HEIGHT=2160
 TW_EXTRA_LANGUAGES := true
 #TW_SCREEN_BLANK_ON_BOOT := true
-#TW_INPUT_BLACKLIST := "hbtp_vm"
+# Blacklist device that is identified as mouse (to hide cursor)
+TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
